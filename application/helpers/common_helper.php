36b61ca99ccababa6b8d89e2689951b592da5ef1
Tabani Timur <taymir@gmail.com>
2011-10-25 18:56:47 +0400

    * сделал фамилии полужирными

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -5,8 +5,8 @@ function colorify_name($name)
     $pos = strpos($name, ' ');
     if($pos !== false)
     {
-        $name = substr_replace($name, ' <span class="nameColored">', $pos, 1);
-        $name .= "</span>";
+        $name = substr_replace($name, '</span> <span class="nameColored">', $pos, 1);
     }
+    $name = '<span class="surnameColored">' . $name . '</span>';
     return $name;
 }
