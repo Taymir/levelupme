ab8e995a16e8d53eec430f104f3f00702b9f7fad
Tabani Timur <taymir@gmail.com>
2011-11-10 01:11:03 +0400

    * форма сверстана, но требует редактирования

--- application/views/pages/registration_view.php
+++ application/views/pages/registration_view.php
@@ -1,59 +1,85 @@
 <?php $this->load->helper('form'); ?>
-<?= form_open('', ''); ?>
+<?= form_open('', 'id="registrationForm" class="niceForm"'); ?>
 <?= form_fieldset('Информация об ученике'); ?>
-<?= form_label('Ф.', 'f_name'); ?>
-<?= form_input('f_name'); ?>
-<?= form_label('И.', 'i_name'); ?>
-<?= form_input('i_name'); ?>
-<?= form_label('О.', 'o_name'); ?>
-<?= form_input('o_name'); ?>
+<p>
+<label>
+<img src="<?= base_url() ?>styles/images/name_symb.png" />Ф. 
+<?= form_input('f_name'); ?> 
+</label>
+<?= form_label('И.', 'i_name'); ?> 
+<?= form_input('i_name'); ?> 
+<?= form_label('О.', 'o_name'); ?> 
+<?= form_input('o_name'); ?> 
+</p>
 
-<?= form_label('Школа', 'school'); ?>
-<?= form_input('school'); ?>
+<p>
+<?= form_label('Школа', 'school'); ?> 
+<?= form_input('school'); ?> 
 
-<?= form_label('Класс', 'class'); ?>
-<?= form_input('class'); ?>
+<?= form_label('Класс', 'class'); ?> 
+<?= form_input('class'); ?> 
+</p>
 <?= form_fieldset_close(); ?>
 
-<?= form_fieldset('Информация о родителе'); ?>
-<?= form_label('Ф.', 'f_pname'); ?>
-<?= form_input('f_pname'); ?>
-<?= form_label('И.', 'i_pname'); ?>
-<?= form_input('i_pname'); ?>
-<?= form_label('О.', 'o_pname'); ?>
-<?= form_input('o_pname'); ?>
-
-<?= form_label('Тел.', 'phone'); ?>
-<?= form_input('phone'); ?>
-
-<?= form_label('E-mail', 'mail_base'); ?>
-<?= form_input('mail_base'); ?>
-<?= form_label('@', 'mail_domain'); ?>
-<?= form_input('mail_domain'); ?>
-<?= form_label('.', 'mail_zone'); ?>
-<?= form_input('mail_zone'); ?>
-
+<?= form_fieldset('Информация о родителе'); ?> 
+<p>
 <label>
-100 р Оценки
-<?= form_radio('tariff', '100', false) ?>
+<img src="<?= base_url() ?>styles/images/name_symb.png" />Ф. 
+<?= form_input('f_pname'); ?> 
 </label>
+<?= form_label('И.', 'i_pname'); ?> 
+<?= form_input('i_pname'); ?> 
+<?= form_label('О.', 'o_pname'); ?> 
+<?= form_input('o_pname'); ?> 
+</p>
 
+<p>
 <label>
-150 р Оценки + необходимая организационная информация
-<?= form_radio('tariff', '150', false) ?>
+<img src="<?= base_url() ?>styles/images/phone_symb.png" />
++7 <?= form_input('phone'); ?> 
 </label>
-
+    
 <label>
-200 р Оценки + организационная и общешкольная информация + аналитический отчет
-<?= form_radio('tariff', '200', false) ?>
+<img src="<?= base_url() ?>styles/images/mail_symb.png" />
+<?= form_input('mail_base'); ?> 
 </label>
-
+<?= form_label('@', 'mail_domain'); ?> 
+<?= form_input('mail_domain', '', 'style="width:50px"'); ?> 
+<?= form_label('.', 'mail_zone'); ?> 
+<?= form_input('mail_zone', '', 'style="width:30px"'); ?> 
+</p>
+<p>
+<div class="tariff">
 <label>
-250 р Оценки + организационная и общешкольная информация + сводка об олимпиадах 
+<img src="<?= base_url() ?>styles/images/tariff_100.png" />
+<strong><?= form_radio('tariff', '100', false) ?>Оценки</strong><br/>
+</label>
+</div>
+
+<div class="tariff">
+<label>
+<img src="<?= base_url() ?>styles/images/tariff_150.png" />
+<strong><?= form_radio('tariff', '150', false) ?>Оценки +</strong><br/> необходимая организационная информация
+</label>
+</div>
+
+<div class="tariff">
+<label>
+<img src="<?= base_url() ?>styles/images/tariff_200.png" />
+<strong><?= form_radio('tariff', '200', false) ?>Оценки +</strong><br/> организационная и общешкольная информация + аналитический отчет
+</label>
+</div>
+
+<div class="tariff">
+<label>
+<img src="<?= base_url() ?>styles/images/tariff_250.png" />
+<strong><?= form_radio('tariff', '250', false) ?>Оценки +</strong><br/> организационная и общешкольная информация + сводка об олимпиадах 
 и соревнованиях + граф-аналитический отчет
-<?= form_radio('tariff', '250', false) ?>
 </label>
+</div>
+</p>
+<?= form_fieldset_close(); ?>
 
 <label>Я ознакомился и принимаю условия <a href="#" target="_blank">договора</a>: <?= form_checkbox('agreement', 'true', false) ?></label>
-<?= form_fieldset_close(); ?>
+<?= form_submit('submit', 'Зарегистрироваться', 'class="btn success"'); ?>
 <?= form_close(); ?>
